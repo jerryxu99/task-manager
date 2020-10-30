9dc9647486ff537d082c45af662cc26ef079fcff
Jerry Xu <jerry.xu99@gmail.com>
2020-10-29 21:41:13 -0700

    Implemented authorization tokens for users to login and logout to access private requests.

--- src/routers/task.js
+++ src/routers/task.js
@@ -1,5 +1,6 @@
 /* eslint-disable consistent-return */
 const express = require('express');
+const { findById } = require('../models/task');
 const Task = require('../models/task');
 
 const router = express.Router();
@@ -48,8 +49,11 @@ router.patch('/tasks/:id', async (req, res) => {
   }
 
   try {
-    const task = await Task.findByIdAndUpdate(req.params.id, req.body,
-      { new: true, runValidators: true });
+    const task = await Task.findById(req.params.id);
+
+    // eslint-disable-next-line no-return-assign
+    updates.forEach((update) => task[update] = req.body[update]);
+    await task.save();
 
     if (!task) {
       return res.status(404).send();
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,6 +1,7 @@
 /* eslint-disable consistent-return */
 const express = require('express');
 const User = require('../models/user');
+const auth = require('../middleware/auth');
 
 const router = new express.Router();
 
@@ -9,22 +10,50 @@ router.post('/users', async (req, res) => {
 
   try {
     await user.save();
-    res.status(201).send(user);
+    const token = await user.generateAuthToken();
+    res.status(201).send({ user, token });
   } catch (e) {
     res.status(400).send(e);
   }
 });
 
-router.get('/users', async (req, res) => {
+router.post('/users/login', async (req, res) => {
   try {
-    const users = await User.find({});
-    res.send(users);
+    const user = await User.findByCredentials(req.body.email, req.body.password);
+    const token = await user.generateAuthToken();
+    res.send({ user, token });
+  } catch (e) {
+    res.status(400).send();
+  }
+});
+
+router.post('/users/logout', auth, async (req, res) => {
+  try {
+    req.user.tokens = req.user.tokens.filter((token) => token.token !== req.token);
+    await req.user.save();
+
+    res.send();
   } catch (e) {
     res.status(500).send();
   }
 });
 
-router.get('/users/:id', async (req, res) => {
+router.post('/users/logoutAll', auth, async (req, res) => {
+  try {
+    req.user.tokens = [];
+    await req.user.save();
+
+    res.send();
+  } catch (e) {
+    res.status(500).send();
+  }
+});
+
+router.get('/users/me', auth, async (req, res) => {
+  res.send(req.user);
+});
+
+router.get('/users/:id', auth, async (req, res) => {
   const { id } = req.params;
 
   try {
@@ -48,8 +77,11 @@ router.patch('/users/:id', async (req, res) => {
   }
 
   try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body,
-      { new: true, runValidators: true });
+    const user = await User.findById(req.params.id);
+
+    // eslint-disable-next-line no-return-assign
+    updates.forEach((update) => user[update] = req.body[update]);
+    await user.save();
 
     if (!user) {
       return res.status(404).send();
