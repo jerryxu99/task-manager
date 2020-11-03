50039e99767f85f2b13c6fec9ab78d4278247d78
Jerry Xu <jerry.xu99@gmail.com>
2020-11-02 22:24:04 -0800

    Added filtering, paginating, and sorting for tasks

--- src/index.js
+++ src/index.js
@@ -25,18 +25,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
 });
-
-const Task = require('./models/task');
-const User = require('./models/user');
-
-const main = async () => {
-  // const task = await Task.findById('5f9ba3553de4ed486a1f25d3');
-  // await task.populate('owner').execPopulate();
-  // console.log(task.owner);
-
-  const user = await User.findById('5f9ba22977a6e0482d4ab317');
-  await user.populate('tasks').execPopulate();
-  console.log(user.tasks);
-};
-
-main();
--- src/routers/task.js
+++ src/routers/task.js
@@ -21,9 +21,34 @@ router.post('/tasks', auth, async (req, res) => {
   }
 });
 
+// GET /tasks?completed=false
+// GET /tasks?limit=10&skip=10
+// GET /tasks?sortBy=createdAt_desc
 router.get('/tasks', auth, async (req, res) => {
+  const match = {};
+  const sort = {};
+
+  if (req.query.completed) {
+    match.completed = req.query.completed === 'true';
+  }
+
+  if (req.query.sortBy) {
+    const parts = req.query.sortBy.split('_');
+    sort[parts[0]] = parts[1] === 'desc' ? -1 : 1;
+  }
+
   try {
-    await req.user.populate('tasks').execPopulate();
+    await req.user
+      .populate({
+        path: 'tasks',
+        match,
+        options: {
+          limit: parseInt(req.query.limit, 10),
+          skip: parseInt(req.query.skip, 10),
+          sort,
+        },
+      })
+      .execPopulate();
     res.send(req.user.tasks);
   } catch (e) {
     res.status(500).send();
@@ -49,21 +74,26 @@ router.get('/tasks/:id', auth, async (req, res) => {
 router.patch('/tasks/:id', auth, async (req, res) => {
   const updates = Object.keys(req.body);
   const allowedUpdates = ['description', 'completed'];
-  const isValidOperation = updates.every((update) => allowedUpdates.includes(update));
+  const isValidOperation = updates.every((update) =>
+    allowedUpdates.includes(update),
+  );
 
   if (!isValidOperation) {
     return res.status(400).send({ error: 'Invalid updates!' });
   }
 
   try {
-    const task = await Task.findOne({ _id: req.params.id, owner: req.user._id });
+    const task = await Task.findOne({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
 
     if (!task) {
       return res.status(404).send();
     }
 
     // eslint-disable-next-line no-return-assign
-    updates.forEach((update) => task[update] = req.body[update]);
+    updates.forEach((update) => (task[update] = req.body[update]));
     await task.save();
 
     res.send(task);
@@ -74,7 +104,10 @@ router.patch('/tasks/:id', auth, async (req, res) => {
 
 router.delete('/tasks/:id', auth, async (req, res) => {
   try {
-    const task = await Task.findOneAndDelete({ _id: req.params.id, owner: req.user._id });
+    const task = await Task.findOneAndDelete({
+      _id: req.params.id,
+      owner: req.user._id,
+    });
 
     if (!task) {
       return res.status(404).send();
