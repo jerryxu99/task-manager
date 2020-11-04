21e8684c13eab947011d190070c5b4a73238a81a
Jerry Xu <jerry.xu99@gmail.com>
2020-11-03 16:46:22 -0800

    included environment variables

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -4,8 +4,11 @@ const User = require('../models/user');
 const auth = async (req, res, next) => {
   try {
     const token = req.header('Authorization').replace('Bearer ', '');
-    const decoded = jwt.verify(token, 'taskmanagerapp');
-    const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await User.findOne({
+      _id: decoded._id,
+      'tokens.token': token,
+    });
 
     if (!user) {
       throw new Error();
